1908df35a6da556f019b8ebc2acd0b84cb078904
David Hecker <heckerdw@gmail.com>
2021-12-28 17:03:17 -0800

    Added vercel.json.

--- api/get_wpm.py
+++ api/get_wpm.py
@@ -1,28 +1,57 @@
+import cgi
 from http.server import BaseHTTPRequestHandler
-from speed_typing.back_end.logic import Logic
-from urllib import parse
-import json
+import io
 
 class handler(BaseHTTPRequestHandler):
 
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'application/json')
-    self.end_headers()
+    def do_POST(self):
+        # Parse the form data posted
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={
+                'REQUEST_METHOD': 'POST',
+                'CONTENT_TYPE': self.headers['Content-Type'],
+            }
+        )
 
-    path = self.path
-    url_components = parse.urlsplit(path)
-    query_string_list = parse.parse_qsl(url_components.query)
-    query = dict(query_string_list)
+        # Begin the response
+        self.send_response(200)
+        self.send_header('Content-Type',
+                         'text/plain; charset=utf-8')
+        self.end_headers()
 
-    logic = Logic()
+        out = io.TextIOWrapper(
+            self.wfile,
+            encoding='utf-8',
+            line_buffering=False,
+            write_through=True,
+        )
 
-    wpm = logic.calculate_wpm(query['original'], query['comparison'], query['time'])
-    accuracy = logic.calculate_accuracy(query.original, query.comparison)
+        out.write('Client: {}\n'.format(self.client_address))
+        out.write('User-agent: {}\n'.format(
+            self.headers['user-agent']))
+        out.write('Path: {}\n'.format(self.path))
+        out.write('Form data:\n')
 
-    obj = {'wpm': f'{wpm}', 'accuracy': f'{accuracy}'}
-    message = json.dumps(obj)
-    message = 'testing'
-    self.wfile.write(message.encode())
+        # Echo back information about what was posted in the form
+        for field in form.keys():
+            field_item = form[field]
+            if field_item.filename:
+                # The field contains an uploaded file
+                file_data = field_item.file.read()
+                file_len = len(file_data)
+                del file_data
+                out.write(
+                    '\tUploaded {} as {!r} ({} bytes)\n'.format(
+                        field, field_item.filename, file_len)
+                )
+            else:
+                # Regular form value
+                out.write('\t{}={}\n'.format(
+                    field, form[field].value))
 
-    return
+        # Disconnect our encoding wrapper from the underlying
+        # buffer so that deleting the wrapper doesn't close
+        # the socket, which is still being used by the server.
+        out.detach()
